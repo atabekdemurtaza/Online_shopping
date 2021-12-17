70434ddffba93a4bb7a19afb54c103dfc2923d18
atabekdemurtaza <mychannel21war@gmail.com>
2021-12-18 03:37:35 +0500

    Filling Angular

--- postclient/src/app/app.module.ts
+++ postclient/src/app/app.module.ts
@@ -5,6 +5,20 @@ import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list.component';
 import { PostDetailComponent } from './post-detail.component';
 import { PostService } from './post.service';
+import { FormModule } from '@angular/forms';
+import { HttpClienModule } from '@angular/common/http';
+import { registerLocaleDate } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+import localeRuExtra from '@angular/common/locales/extra/ru';
+import { LOCALE_ID } from '@angular/core';
+import { Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: ':pk', component: PostDetailComponent},
+  {path: '', component: PostListComponent}
+];
+
+registerLocaleDate(localeRu, 'ru', localeRuExtra)
 
 
 @NgModule({
@@ -14,10 +28,13 @@ import { PostService } from './post.service';
     PostDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClienModule,
+    FormModule
   ],
   providers: [
-    PostService
+    PostService,
+    {provide: LOCALE_ID, useValue: 'ru'}
   ],
   bootstrap: [AppComponent]
 })
